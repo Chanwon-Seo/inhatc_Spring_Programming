70b2f069dbb9029c26a148141da4855858d0faf7
Chanwon-Seo <90185805+Chanwon-Seo@users.noreply.github.com>
2022-06-09 07:00:36 +0900

    SearchCriteria

---
+++ board/src/main/java/com/inhatc/domain/SearchCriteria.java
@@ -0,0 +1,16 @@
+package com.inhatc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SearchCriteria extends Criteria {
+
+    private String searchType;
+    private String keyword;
+}
+
+
